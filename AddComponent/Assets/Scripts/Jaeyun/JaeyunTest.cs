0e7811b764aeab76bc7c0eba7cab61bbf172055f
wodbs0519 <48546353+wodbs0519@users.noreply.github.com>
2020-08-20 23:51:16 +0900

    asd

--- AddComponent/Assets/Scripts/Jaeyun/JaeyunTest.cs
+++ AddComponent/Assets/Scripts/Jaeyun/JaeyunTest.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SImpleTest : MonoBehaviour
+public class JaeyunTest : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
