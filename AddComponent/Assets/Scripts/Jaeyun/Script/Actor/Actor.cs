1ced6008446088b54b9361f2d2964ce540cb57a6
YesGGi <rubickbh@gmail.com>
2020-08-22 00:04:13 +0900

    actor 수정
    ㅇㅇ

--- AddComponent/Assets/Scripts/Jaeyun/Script/Actor/Actor.cs
+++ AddComponent/Assets/Scripts/Jaeyun/Script/Actor/Actor.cs
@@ -1,6 +1,7 @@
 ﻿using IndieMarc.Platformer;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityTemplateProjects.Jaeyun.Script.Development_Tool;
 
 namespace UnityTemplateProjects.Jaeyun.Script.Actor
@@ -28,11 +29,9 @@ namespace UnityTemplateProjects.Jaeyun.Script.Actor
         public float move_deccel = 20f;
         public float move_max = 5f;
 
-
         [Header("Jump")]
         public bool can_jump = true;
         public bool double_jump = true;
-        //public bool jump_on_enemies = true;
         public float jump_strength = 5f;
         public float jump_time_min = 0.4f;
         public float jump_time_max = 0.8f;
@@ -44,41 +43,79 @@ namespace UnityTemplateProjects.Jaeyun.Script.Actor
 
 
 
+        [Header("Climb")]
+        public float climb_speed = 2f;
+
+        public UnityAction onDeath;
+        public UnityAction onHit;
+        public UnityAction onJump;
+        public UnityAction onLand;
+        public UnityAction onClimb;
 
         private Rigidbody2D rigid;
         private CapsuleCollider2D capsule_coll;
         private ContactFilter2D contact_filter;
         private Vector2 coll_start_h;
         private Vector2 coll_start_off;
+        private Vector3 start_scale;
+        private Vector3 last_ground_pos;
+
+
+        private PlayerCharacterState state;
+        private Vector2 move;
 
         private bool was_grounded = false;
         private bool is_grounded = false;
         private bool is_fronted = false;
         private bool is_jumping = false;
         private bool is_double_jump = false;
+        private Vector3 grab_pos;
 
-        private Vector2 move;
+        private float state_timer = 0f;
+        private float jump_timer = 0f;
+        private float hit_timer = 0f;
 
+        private static List<Actor> character_list = new List<Actor>();
 
-        private void Awake()
+        void Awake()
         {
+
             _myMat = GetComponent<SpriteRenderer>().material;
             _defaultShader = Shader.Find("Custom/2D Sprite");
             _OutlineShader = Shader.Find("Shader Graphs/2D DrawOutline");
 
-            rigid = this.GetComponent<Rigidbody2D>();
+            character_list.Add(this);
+            rigid = GetComponent<Rigidbody2D>();
             capsule_coll = GetComponent<CapsuleCollider2D>();
             coll_start_h = capsule_coll.size;
             coll_start_off = capsule_coll.offset;
+            start_scale = transform.localScale;
+            //average_ground_pos = transform.position;
+            last_ground_pos = transform.position;
 
+            contact_filter = new ContactFilter2D();
+            contact_filter.layerMask = raycast_mask;
+            contact_filter.useLayerMask = true;
+            contact_filter.useTriggers = false;
 
         }
 
-        private void FixedUpdate()
+        void OnDestroy()
         {
-            //움직임.
-            ControlManager controls = ControlManager.Get(player_id);
+            character_list.Remove(this);
+        }
 
+
+
+        //Handle physics
+        void FixedUpdate()
+        {
+            if (TheGame.IsGamePaused())
+                return;
+
+            PlayerControls controls = PlayerControls.Get(player_id);
+
+            //Movement velocity
             Vector3 move_input = controls.GetMove();
             float desiredSpeed = Mathf.Abs(move_input.x) > 0.1f ? move_input.x * move_max : 0f;
             float acceleration = Mathf.Abs(move_input.x) > 0.1f ? move_accel : move_deccel;
@@ -89,6 +126,187 @@ namespace UnityTemplateProjects.Jaeyun.Script.Actor
             is_grounded = DetectObstacle(Vector3.down);
             is_fronted = IsFronted();
 
+            if (state == PlayerCharacterState.Normal)
+            {
+                UpdateFacing();
+                UpdateJump();
+
+                //Move
+                move.x = is_fronted ? 0f : move.x;
+                rigid.velocity = move;
+
+                CheckForFloorTrigger();
+            }
+
+            if (state == PlayerCharacterState.Climb)
+            {
+                move = controls.GetMove() * climb_speed;
+                rigid.velocity = move;
+            }
+
+            if (state == PlayerCharacterState.Dead)
+            {
+                move.x = 0f;
+                UpdateJump(); //Keep falling
+                rigid.velocity = move;
+            }
+        }
+
+        //Handle render and controls
+        void Update()
+        {
+            if (TheGame.IsGamePaused())
+                return;
+
+            hit_timer += Time.deltaTime;
+            state_timer += Time.deltaTime;
+            //grounded_timer += Time.deltaTime;
+
+            //Controls
+            PlayerControls controls = PlayerControls.Get(player_id);
+
+            if (state == PlayerCharacterState.Normal)
+            {
+                if (controls.GetJumpDown())
+                    Jump();
+
+                Ladder ladder = Ladder.GetOverlapLadder(gameObject);
+                if (ladder && controls.GetMove().y > 0.1f && state_timer > 0.7f)
+                    Climb();
+
+
+            }
+
+            if (state == PlayerCharacterState.Climb)
+            {
+                Ladder ladder = Ladder.GetOverlapLadder(gameObject);
+                if (ladder == null)
+                {
+                    state = PlayerCharacterState.Normal;
+                    state_timer = 0f;
+                }
+
+                if (controls.GetJumpDown())
+                    Jump(true);
+            }
+
+            //Reset when fall
+
+
+        }
+
+        private void UpdateFacing()
+        {
+            if (Mathf.Abs(move.x) > 0.01f)
+            {
+                float side = (move.x < 0f) ? -1f : 1f;
+                transform.localScale = new Vector3(start_scale.x * side, start_scale.y, start_scale.z);
+            }
+        }
+
+        private void UpdateJump()
+        {
+            PlayerControls controls = PlayerControls.Get(player_id);
+
+            //Jump
+            jump_timer += Time.fixedDeltaTime;
+
+            //Jump end timer
+            if (is_jumping && !controls.GetJumpHold() && jump_timer > jump_time_min)
+                is_jumping = false;
+            if (is_jumping && jump_timer > jump_time_max)
+                is_jumping = false;
+
+            //Jump hit ceil
+
+
+            //Add jump velocity
+            if (!is_grounded)
+            {
+                //Falling
+                float gravity = !is_jumping ? jump_fall_gravity : jump_gravity; //떨어질때 중력상승
+                move.y = Mathf.MoveTowards(move.y, -move_max * 2f, gravity * Time.fixedDeltaTime);
+            }
+            else if (!is_jumping)
+            {
+                //Grounded
+                move.y = 0f;
+            }
+
+
+
+            if (!was_grounded && is_grounded)
+            {
+                if (onLand != null)
+                    onLand.Invoke();
+            }
+        }
+
+
+
+        public void Jump(bool force_jump = false)
+        {
+            if (can_jump && (!force_jump))
+            {
+                if (is_grounded || force_jump || (!is_double_jump && double_jump))
+                {
+                    is_double_jump = !is_grounded;
+                    move.y = jump_strength;
+                    jump_timer = 0f;
+                    is_jumping = true;
+                    state = PlayerCharacterState.Normal;
+                    state_timer = 0f;
+                    if (onJump != null)
+                        onJump.Invoke();
+                }
+            }
+        }
+
+        public void Climb()
+        {
+            state = PlayerCharacterState.Climb;
+            state_timer = 0f;
+
+            if (onClimb != null)
+                onClimb.Invoke();
+        }
+
+        private void CheckForFloorTrigger()
+        {
+            //Platform
+            Vector3 center = GetCapsulePos(Vector3.down);
+            float radius = GetCapsuleRadius() + ground_raycast_dist;
+            GameObject platform = RaycastObstacle<PlatformMoving>(center, Vector3.down * radius);
+            if (platform && platform.GetComponent<PlatformMoving>())
+            {
+                PlatformMoving pmoving = platform.GetComponent<PlatformMoving>();
+                pmoving.OnCharacterStep();
+                rigid.position += new Vector2(pmoving.GetMove().x, pmoving.GetMove().y) * Time.fixedDeltaTime;
+            }
+
+            //Enemy
+            GameObject enemy_trigger = RaycastObstacle<Enemy>(center, Vector3.down * radius);
+            if (enemy_trigger && enemy_trigger.GetComponent<Enemy>())
+            {
+                Enemy etrigger = enemy_trigger.GetComponent<Enemy>();
+                TouchEnemy(etrigger);
+            }
+
+            //Floor trigger
+            GameObject floor_trigger = RaycastObstacle<FloorTrigger>(center, Vector3.down * radius);
+            if (floor_trigger && floor_trigger.GetComponent<FloorTrigger>())
+            {
+                FloorTrigger ftrigger = floor_trigger.GetComponent<FloorTrigger>();
+                ftrigger.Activate();
+            }
+        }
+
+        private bool IsFronted()
+        {
+            bool obstacle = DetectObstacle(GetFacing());
+            bool box = RaycastObstacle<Box>(GetCapsulePos(GetFacing()), GetFacing());
+            bool enemy = RaycastObstacle<Enemy>(GetCapsulePos(GetFacing()), GetFacing());
+            return obstacle && !box && !enemy;
         }
 
         private bool DetectObstacle(Vector3 dir)
@@ -123,35 +341,6 @@ namespace UnityTemplateProjects.Jaeyun.Script.Actor
         }
 
 
-        private bool IsFronted()
-        {
-            bool obstacle = DetectObstacle(GetFacing());
-            bool box = RaycastObstacle<Box>(GetCapsulePos(GetFacing()), GetFacing());
-            bool enemy = RaycastObstacle<Enemy>(GetCapsulePos(GetFacing()), GetFacing());
-            return obstacle && !box && !enemy;
-        }
-        public Vector2 GetFacing()
-        {
-            return Vector2.right * Mathf.Sign(transform.localScale.x);
-        }
-
-        public Vector2 GetSize()
-        {
-            if (capsule_coll != null)
-                return new Vector2(Mathf.Abs(transform.localScale.x) * capsule_coll.size.x, Mathf.Abs(transform.localScale.y) * capsule_coll.size.y);
-            return new Vector2(Mathf.Abs(transform.localScale.x), Mathf.Abs(transform.localScale.y));
-        }
-        private float GetCapsuleRadius()
-        {
-            return GetSize().x * 0.5f;
-        }
-        private Vector3 GetCapsulePos(Vector3 dir)
-        {
-            Vector2 orientation = dir.normalized;
-            Vector2 raycast_offset = capsule_coll.offset + orientation * Mathf.Abs(capsule_coll.size.y * 0.5f - capsule_coll.size.x * 0.5f);
-            return rigid.position + raycast_offset * capsule_coll.transform.lossyScale.y;
-        }
-
         public bool RaycastObstacle(Vector2 pos, Vector2 dir)
         {
             RaycastHit2D[] hitBuffer = new RaycastHit2D[5];
@@ -180,6 +369,163 @@ namespace UnityTemplateProjects.Jaeyun.Script.Actor
             }
             return null;
         }
+
+        public void Teleport(Vector3 pos)
+        {
+            transform.position = pos;
+            last_ground_pos = pos;
+            move = Vector2.zero;
+            is_jumping = false;
+        }
+
+        public void SetRespawnPoint(Vector3 pos)
+        {
+            last_ground_pos = pos;
+        }
+
+
+
+
+        public void Kill()
+        {
+            if (!IsDead())
+            {
+                state = PlayerCharacterState.Dead;
+                rigid.velocity = Vector2.zero;
+                move = Vector2.zero;
+                state_timer = 0f;
+                capsule_coll.enabled = false;
+
+                if (onDeath != null)
+                    onDeath.Invoke();
+            }
+        }
+
+        public PlayerCharacterState GetState()
+        {
+            return state;
+        }
+
+        public Vector2 GetMove()
+        {
+            return move;
+        }
+
+        public Vector2 GetFacing()
+        {
+            return Vector2.right * Mathf.Sign(transform.localScale.x);
+        }
+
+        public bool IsJumping()
+        {
+            return is_jumping;
+        }
+
+        public bool IsGrounded()
+        {
+            return is_grounded;
+        }
+
+
+
+        public bool IsClimbing()
+        {
+            return state == PlayerCharacterState.Climb;
+        }
+
+
+
+        public bool IsDead()
+        {
+            return state == PlayerCharacterState.Dead;
+        }
+
+        public Vector2 GetSize()
+        {
+            if (capsule_coll != null)
+                return new Vector2(Mathf.Abs(transform.localScale.x) * capsule_coll.size.x, Mathf.Abs(transform.localScale.y) * capsule_coll.size.y);
+            return new Vector2(Mathf.Abs(transform.localScale.x), Mathf.Abs(transform.localScale.y));
+        }
+
+        private Vector3 GetCapsulePos(Vector3 dir)
+        {
+            Vector2 orientation = dir.normalized;
+            Vector2 raycast_offset = capsule_coll.offset + orientation * Mathf.Abs(capsule_coll.size.y * 0.5f - capsule_coll.size.x * 0.5f);
+            return rigid.position + raycast_offset * capsule_coll.transform.lossyScale.y;
+        }
+
+        private float GetCapsuleRadius()
+        {
+            return GetSize().x * 0.5f;
+        }
+
+        private void TouchEnemy(Enemy enemy)
+        {
+            Vector3 diff = GetCapsulePos(Vector3.down) - enemy.transform.position;
+            if (diff.y > 0f)
+            {
+                Jump(true); //Bounce on enemy
+                enemy.Kill(); //Kill enemy
+            }
+            else
+            {
+              //  TakeDamage(enemy.damage);
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (IsDead())
+                return;
+
+            Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                TouchEnemy(enemy);
+            }
+        }
+
+        public static Actor GetNearest(Vector3 pos, float range = 99999f, bool alive_only = false)
+        {
+            Actor nearest = null;
+            float min_dist = range;
+            foreach (Actor character in GetAll())
+            {
+                if (!alive_only || !character.IsDead())
+                {
+                    float dist = (pos - character.transform.position).magnitude;
+                    if (dist < min_dist)
+                    {
+                        min_dist = dist;
+                        nearest = character;
+                    }
+                }
+            }
+            return nearest;
+        }
+
+        public static Actor Get(int player_id)
+        {
+            foreach (Actor character in GetAll())
+            {
+                if (character.player_id == player_id)
+                {
+                    return character;
+                }
+            }
+            return null;
+        }
+
+        public static List<Actor> GetAll()
+        {
+            return character_list;
+        }
+
+
+
+
+
+
 
 
 
---
+++ AddComponent/Assets/Scripts/이예석/manager/CharacterAni.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityTemplateProjects.Jaeyun.Script.Actor
+{
+    [RequireComponent(typeof(Actor))]
+    [RequireComponent(typeof(Animator))]
+
+    public class CharacterAni : MonoBehaviour
+    {
+        private Actor character;
+        private SpriteRenderer render;
+        private Animator animator;
+        private float flash_fx_timer;
+
+        void Awake()
+        {
+            character = GetComponent<Actor>();
+            render = GetComponent<SpriteRenderer>();
+            animator = GetComponent<Animator>();
+
+            character.onJump += OnJump;
+            character.onHit += OnDamage;
+            character.onDeath += OnDeath;
+            character.onClimb += OnClimb;
+        }
+
+
+        void Update()
+        {
+
+            //Anims
+            animator.SetBool("Jumping", character.IsJumping());
+            animator.SetBool("InAir", !character.IsGrounded());
+            animator.SetFloat("Speed", character.GetMove().magnitude);
+            animator.SetBool("Climb", character.IsClimbing());
+
+            //Hit flashing
+            render.color = new Color(render.color.r, render.color.g, render.color.b, 1f);
+            if (flash_fx_timer > 0f)
+            {
+                flash_fx_timer -= Time.deltaTime;
+                float alpha = Mathf.Abs(Mathf.Sin(flash_fx_timer * Mathf.PI * 4f));
+                render.color = new Color(render.color.r, render.color.g, render.color.b, alpha);
+            }
+        }
+
+        void OnClimb()
+        {
+            animator.SetTrigger("ChangeState");
+            animator.SetBool("Climb", true);
+        }
+
+        void OnCrouch()
+        {
+            animator.SetTrigger("Crouch");
+        }
+
+        void OnJump()
+        {
+            animator.SetTrigger("Jump");
+            animator.SetBool("Jumping", true);
+        }
+
+        void OnDamage()
+        {
+            if (!character.IsDead())
+                flash_fx_timer = 1f;
+        }
+
+        void OnDeath()
+        {
+            animator.SetTrigger("Death");
+        }
+    }
+
+}
