651da73331a7063c92a70bb76b028c31daf574e2
wodbs0519 <48546353+wodbs0519@users.noreply.github.com>
2020-08-23 10:45:52 +0900

    asdsda

---
+++ AddComponent/Assets/Scripts/Jaeyun/Script/MakeButton.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityTemplateProjects.Jaeyun.Script.Development_Tool;
+using Random = UnityEngine.Random;
+
+namespace UnityTemplateProjects.Jaeyun.Script
+{
+    public class MakeButton : MonoBehaviour
+    {
+
+        public bool isMake;
+        public class MakeButtonsInfos
+        {
+            public ComponentType componentType;
+            public float makeMinTime;
+            public float makeMaxTime;
+        }
+
+        public List<MakeButtonsInfos> makeButtonsInfoses = new List<MakeButtonsInfos>();
+
+        public void StartMake()
+        {
+            foreach (var makeButtonsInfose in makeButtonsInfoses)
+            {
+                var buttonPrefab = makeButtonsInfose.componentType.buttonPrefab;
+                StartCoroutine(MakeButtonRoutine(makeButtonsInfose.makeMinTime, makeButtonsInfose.makeMaxTime, buttonPrefab));
+            }
+        }
+
+        public void StopMake()
+        {
+            StopAllCoroutines();
+        }
+
+        IEnumerator MakeButtonRoutine(float minTime, float maxTime, ComponentButton buttonPrefab)
+        {
+            while (true)
+            {
+                var randomTime = Random.Range(minTime, maxTime);
+                yield return new WaitForSeconds(randomTime);
+
+                
+                //이 부분 오브젝트 풀링 써주시는게 좋을듯?
+                Instantiate(buttonPrefab);
+            }
+        }
+        
+    }
+}
