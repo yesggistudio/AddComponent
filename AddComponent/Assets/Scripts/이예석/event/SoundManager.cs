c38c743a4d4246a76788f5b297f9032ee6e5d6da
YesGGi <rubickbh@gmail.com>
2020-08-23 11:07:29 +0900

    ㅇㅇㄹㄴㄹㄴ
    ㅇㅇㅇㄹㄴㄹㄴ

---
+++ AddComponent/Assets/Scripts/이예석/event/SoundManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager instance;
+    public static SoundManager Instance { get { return instance; } }
+
+
+
+
+    public AudioClip[] soundeffect = new AudioClip[10];
+
+    public AudioSource effectsource;
+
+    
+
+
+    private void Awake()
+    {
+        if (instance)
+        {
+            DestroyImmediate(gameObject);
+            return;
+        }
+        instance = this;
+    }
+
+
+
+    public void SoundFx(int t)
+    {
+        
+      //  soundeffect[t]
+
+    }
+
+
+
+
+}
